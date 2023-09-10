e727e5548719845c5d659f3dc649b76116372427
Derek Swanson <drs0057@auburn.edu>
2023-09-10 14:30:31 -0500

    Implemented smart input field for guesses.

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from random import randint
 # from database import db
 # from models import Users
 # source ~/.bashrc
+# source .venv_music/bin/activate
+# .venv_music/bin/python app.py
 
 app = Flask(__name__)
 app.secret_key = "Tigers-315700"
@@ -28,6 +30,7 @@ class Users(db.Model):
         self.username = username
         self.hash_pw = hash_pw
 
+
 def duplicate_username(username):
     """Determines if a username already exists in the db."""
     users = Users.query.all()
@@ -35,6 +38,7 @@ def duplicate_username(username):
         if user.username == username:
             return True
     return False
+
 
 def find_user(username):
     """Returns the corresponding user object from the db."""
@@ -44,6 +48,7 @@ def find_user(username):
             return user
     return None
 
+
 def add_token_to_session(code):
     """Adds the access token to the session."""
     if "access_token" in session:
@@ -52,12 +57,14 @@ def add_token_to_session(code):
         access_token, refresh_token = api.request_access_refresh_token(code)
         session["access_token"] = access_token
 
+
 progress = 0
 @app.route('/progress')
 def get_progress():
     """Returns progress used to make progress bar."""
     global progress
     return jsonify(progress=progress)
+
 
 def songs_by_artist(artist, token):
     global progress
@@ -136,13 +143,18 @@ def logout():
         flash("You are not logged in.")
         return redirect(url_for("login"))
     
+
 @app.route("/play", methods=["POST", "GET"])
 def play():
     code = request.args.get('code', '')
     add_token_to_session(code)
     if request.method == "POST":
-        if 'artist-field' in request.form:
-            session['artist-field'] = request.form['artist-field']
+        # if 'song-num-field' in request.form:
+        #     print(f'Number: {request.form["song-num-field"]}')
+        #     # session['song_num'] = request.form['number-field']
+        #     # return jsonify(message="Number gathered successfully."), 200
+        if 'artist-name-field' in request.form:
+            session['artist-name-field'] = request.form['artist-name-field']
             return redirect(url_for("play_artist"))
         if 'shuffleLibrary' in request.form:
             return redirect(url_for("play_library"))
@@ -158,25 +170,28 @@ def play():
             flash("You must log in first to play.")
             return redirect(url_for("login"))
 
+
 @app.route("/play/artist", methods=["POST", "GET"])
 def play_artist():
     if request.method == "POST":
         pass
     else:
         if "username" in session:
-            songs = songs_by_artist(session["artist-field"], session["access_token"])
+            # TOGGLE
+            songs = songs_by_artist(session["artist-name-field"], session["access_token"])
             # songs = [{'name': 'The Age of Worry', 'uri': 'spotify:track:1RywwImkBFUEVcRTBmw7vL', 'image_url': 'https://i.scdn.co/image/ab67616d0000b2733c6bbf44de57c6eb51818694'}, {'name': 'Shot in the Dark', 'uri': 'spotify:track:239yM7BAQ2CkNc61ogPGXo', 'image_url': 'https://i.scdn.co/image/ab67616d0000b273779063301154e835a91a35e0'}]
             if songs == []:
-                flash(f'You do not have any liked songs by the artist "{session["artist-field"]}".')
-                session.pop("artist-field", None)
+                flash(f'You do not have any liked songs by the artist "{session["artist-name-field"]}".')
+                session.pop("artist-name-field", None)
                 return redirect(url_for("play"))
             else:
-                session.pop("artist-field", None)
+                session.pop("artist-name-field", None)
                 return render_template("play_artist.html", songs=songs, \
                 token=session["access_token"])
         else:
             flash("You must log in first to play.")
             return redirect(url_for("login"))
+
 
 @app.route("/account", methods=["POST", "GET"])
 def account():
@@ -187,6 +202,7 @@ def account():
         flash("You must log in to view account information.")
         return redirect(url_for("login"))
     
+
 @app.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == "POST":
@@ -212,6 +228,7 @@ def register():
     else:
         return render_template("register.html")
     
+
 @app.route("/game_data")
 def game_data():
     if "username" in session:
