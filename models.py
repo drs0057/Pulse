0f806d4b8d48781d0f75ae3a345f28d74a3d9751
Derek Swanson <drs0057@auburn.edu>
2023-12-29 12:51:31 -0600

    Small changes to model and notes.

--- models.py
+++ models.py
@@ -10,19 +10,6 @@ class Users(db.Model):
     password_hash = Column(String(255), nullable=False)
 
 
-class Games(db.Model):
-    id = Column(Integer, primary_key=True, nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-
-
-class Guesses(db.Model):
-    id = Column(Integer, primary_key=True, nullable=False)
-    time_to_guess = Column(Integer, nullable=False)
-    is_correct = Column(Boolean, default=False, nullable=False)
-    game_id = Column(Integer, ForeignKey("games.id"), nullable=False)
-    song_id = Column(Integer, ForeignKey("songs.id"), nullable=False)
-
-
 class Artists(db.Model):
     id = Column(Integer, primary_key=True, nullable=False)
     artist_name = Column(String(255), nullable=False)
@@ -34,12 +21,23 @@ class Albums(db.Model):
     artist_id = Column(Integer, ForeignKey("artists.id"), nullable=False)
 
 
-class Songs(db.Model):
+class Games(db.Model):
     id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+
+class Songs(db.Model):
+    spotify_id = Column(String(255), primary_key=True, nullable=False)
     song_name = Column(String(255), nullable=False)
     album_id = Column(Integer, ForeignKey("albums.id"), nullable=False)
     artist_id = Column(Integer, ForeignKey("artists.id"), nullable=False)
 
+
+class Guesses(db.Model):
+    id = Column(Integer, primary_key=True, nullable=False)
+    time_to_guess = Column(Integer)
+    game_id = Column(Integer, ForeignKey("games.id"), nullable=False)
+    spotify_id = Column(String(255), ForeignKey("songs.spotify_id"), nullable=False)
 
 
 
